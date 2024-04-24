a0f0f8c754b2c20c7704e407e584c4ed9636f957
Aryan Singhal <aryans8791@gmail.com>
2024-04-24 18:05:57 +0530

    Added Boilerplate

---
+++ src/components/AddUser.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function AddUser({ dispatch, setBalance, balance }) {
+    const [name, setName] = useState("");
+
+    function handleClick(e) {
+        e.preventDefault();
+        setName("");
+    }
+
+    return (
+        <form>
+            <label htmlFor="name">Enter the Name:</label>
+            <input
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+                type="text"
+                value={name}
+            />
+            <button onClick={handleClick} id="add-user-button">
+                Add User
+            </button>
+        </form>
+    );
+}
+
+export default AddUser;
---
+++ src/components/Users.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Users({ users, dispatch, setBalance, balance }) {
+    function handleFuel() {
+    }
+    return (
+        <div>
+            <button onClick={() => handleFuel()} id="fill-fuel">
+                Fill Fuel
+            </button>
+            {users.map((user) => {
+                return (
+                    <div key={user.id} className="user">
+                        {user.name}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Users;
